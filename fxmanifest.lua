9008a2dcb08dd6e66e2f790de7f6fc93db67069b
Noel <108223454+Test1calCutter@users.noreply.github.com>
2023-08-03 21:15:45 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+
+game 'gta5'
+author 'OnlyScripts / veryappropriatename'
+description 'Arma 3 Themed Loadingscreen by OnlyScripts.'
+
+files {
+    'index.html',
+    'img/*.png',
+    'style.css'
+}
+
+loadscreen 'index.html'
+lua54 'yes'
